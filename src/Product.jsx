5dbaf28d99c3a5b34e94ab19f2b66a89c95691b5
Hritik Dubey <dubeyhritik305@gmail.com>
2026-07-07 00:56:21 +0530

    Day 1: React basics, JSX, components, import/export and styling

---
+++ src/Product.jsx
@@ -0,0 +1,13 @@
+import "./Product.css";
+
+function Product() {
+  return (
+    <div className="product">
+      <h3>Product Title</h3>
+      <h5>Price : ₹999</h5>
+      <p>Description of the product.</p>
+    </div>
+  );
+}
+
+export default Product;
---
+++ src/Title.jsx
@@ -0,0 +1,5 @@
+function Title() {
+    return <h2>Learning React 🚀</h2>;
+}
+
+export default Title;
